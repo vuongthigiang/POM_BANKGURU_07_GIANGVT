3fad577f7e835fa7208f56446348a46402f48623
vuongthigiang@f88.vn <vuongthigiang@f88.vn>
2019-05-15 11:01:54 +0700

    POM

---
+++ testcases/com/bankguru/login/Login_01_ValidateLoginForm.java
@@ -0,0 +1,21 @@
+package com.bankguru.login;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Login_01_ValidateLoginForm {
+	
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void f() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
